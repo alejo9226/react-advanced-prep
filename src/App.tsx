2722c93b44b18e4130f0a8b8cd5cafea05cd7f28
Alejandro Alfaro <alejo9226@gmail.com>
2025-01-16 16:39:51 -0500

    Practice: React.memo, useCallback 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { OptimizedListMemoized } from './components/OptimizedListMemoized'
+import { OptimizedListNotMemoized } from './components/OptimizedListNotMemoized'
 
 function App() {
 
   return (
     <>
+      <OptimizedListMemoized />
+      <OptimizedListNotMemoized />
     </>
   )
 }
